44b0e5250b3f6fa798a46b4d35a6e3eae5d08399
Renato Vicente <revi66@hotmail.com>
2020-08-01 14:38:03 -0300

    feat: add route navigation guard

--- src/router.js
+++ src/router.js
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { getUser } from './service/auth.js'
 const Dashboard= () => import('./views/Dashboard.vue')
 const Login = () => import('./views/Login.vue')
 const Signup = () => import('./views/Signup.vue')
 const NotFound = () => import('./views/NotFound.vue')
+
 export const router = createRouter({
   history: createWebHistory(),
   strict: true,
@@ -32,16 +34,17 @@ export const router = createRouter({
   ]
 })
 
-// router.beforeEach((to, from, next) => {
-//   if (to.name === from.name) {
-//     next()
-//   } else {
-//     if (to.matched.some(record => record.meta.requiresAuth) && to.meta.requiresAuth === undefined) {
-//       store.getters['auth/user'].id === null
-//         ? next({ name: 'Login' })
-//         : next()
-//     } else {
-//       next()
-//     }
-//   }
-// })
+router.beforeEach((to, from, next) => {
+  if (to.name === from.name) {
+    next()
+  } else {
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+      const user = getUser()
+      user === null
+        ? next({ name: 'Login' })
+        : next()
+    } else {
+      next()
+    }
+  }
+})
