0b9465bfcb26f79107a04182a12bc8238ebba68d
Renato Vicente <revi66@hotmail.com>
2020-08-01 00:49:00 -0300

    style: organize components on screen

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,30 +1,83 @@
 <template>
-  <t-card class="child-view">
-    <h1>Login</h1>
-    <t-input-text v-model="username" type="email" name="username" label="Email" placeholder="example@ticketpass.com" />
-    <t-input-text v-model="password" type="password" name="password" placeholder="">
-      <template v-slot:label>
-        <div class="space-between">
-          <span>Password</span>
-          <a href="/forgot-password" class="forgot-password">Forgot password?</a>
-        </div>
-      </template>
-    </t-input-text>
+  <t-card class="child-view t-login">
+    <div class="t-login__logo">
+      <img src="https://ticketpass.org/_nuxt/dist/948663e9104105656d0c7334673f7c7f.svg" alt="ticketpass logo" />
+    </div>
+    <form id="login" @submit.prevent="login" class="t-login-form">
+      <div class="t-login-form__input">
+        <t-input-text
+          v-model="username"
+          type="email"
+          id="username"
+          name="username"
+          label="Email"
+          placeholder="example@ticketpass.com"
+          tabindex="1"
+        />
+      </div>
+      <div class="t-login-form__input">
+        <t-input-text
+          v-model="password"
+          type="password"
+          id="password"
+          name="password"
+          placeholder
+          tabindex="2"
+        >
+          <template v-slot:label>
+            <div class="space-between">
+              <span>Password</span>
+              <router-link to="/not-found" class="forgot-password" tabindex="4">Forgot password?</router-link>
+            </div>
+          </template>
+        </t-input-text>
+      </div>
+      <div class="t-login-form__button">
+        <button tabindex="3" id="login-button">submit</button>
+      </div>
+    </form>
   </t-card>
 </template>
 
 <script>
 export default {
-  name: 'Login',
-  data () {
+  name: "Login",
+  data() {
     return {
-      username: '',
-      password: ''
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    login() {
+      console.log("login");
+    },
+  },
+};
+</script>
+<style lang="scss">
+.t-login {
+  width: 300px;
+  padding: 30px 15px;
+  &__logo {
+    text-align: center;
+    > img {
+      margin: 10px 0px;
+      width: 200px;
     }
   }
 }
-</script>
-<style>
+.t-login-form {
+  margin: auto;
+  width: 250px;
+  &__input {
+    margin: 20px 0px;
+  }
+  &__button {
+    margin: 5px 0px;
+    text-align: center;
+  }
+}
 .space-between {
   display: flex;
   justify-content: space-between;
