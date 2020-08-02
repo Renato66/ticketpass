e9ec77ffea507081d3a7c4d8badd78426efe2d04
Renato Vicente <revi66@hotmail.com>
2020-08-02 01:42:36 -0300

    feat: add modal component

---
+++ src/components/core/TModal.vue
@@ -0,0 +1,54 @@
+<template>
+  <teleport to="body">
+    <transition name="fade" mode="out-in">
+      <div class="t-modal" v-show="modelValue">
+        <div class="t-modal__backdrop" @click="$emit('update:modelValue', false)"></div>
+        <transition name="slide-left" >
+          <div class="t-modal__content">
+            <slot />
+          </div>
+        </transition>
+      </div>
+    </transition>
+  </teleport>
+</template>
+
+<script>
+export default {
+  props: {
+    modelValue: {
+      type: Boolean
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.t-modal {
+  overflow: hidden;
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  padding: 30px;
+  z-index: 99;
+  display: grid;
+  place-items: center;
+  overflow: auto;
+  transition: opacity .5s;
+  &__backdrop {
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    z-index: -1;
+    background: #04040429;
+  }
+  &__content {
+    margin-bottom: 30px;
+    max-width: 90%;
+  }
+}
+</style>
--- src/plugins/core-components.js
+++ src/plugins/core-components.js
@@ -3,10 +3,12 @@ import TCard from '../components/core/TCard.vue'
 import TCheckbox from '../components/core/TCheckbox.vue'
 import TInputText from '../components/core/TInputText.vue'
 import TLoading from '../components/core/TLoading.vue'
+import TModal from '../components/core/TModal.vue'
 export default (app) => {
   app.component('t-btn', TBtn)
   app.component('t-card', TCard)
   app.component('t-checkbox', TCheckbox)
   app.component('t-input-text', TInputText)
   app.component('t-loading', TLoading)
+  app.component('t-modal', TModal)
 }
